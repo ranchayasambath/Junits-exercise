0703576ade71af327e0e305df937c5f79b2f8e03
ranchayasambath <ranchayasambath@gmail.com>
2022-03-14 14:13:24 -0500

    initial commit

---
+++ src/test/java/JUnitTests.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class JUnitTests {
+
+    @Test
+    public void testIfNameIsKat(){
+        String expected = "Jack";
+        String actual = "jack";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIfChangeIsCorrect(){
+        Double price = 10.0;
+        Double discount = 4.5;
+
+        assertEquals(5.0, price-discount, 0.5); // delta: margin of error
+        assertNotEquals(4.2, price-discount, 0.5);
+    }
+}
